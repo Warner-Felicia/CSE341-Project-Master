67ca334814d798e880d38ac8bbddc8042bf4beb6
Warner-Felicia <foodleswarner@gmail.com>
2021-09-16 20:30:37 -0400

    Corrected validation errors on prove01

--- prove01-routes.js
+++ prove01-routes.js
@@ -5,18 +5,19 @@ const routesHandler = (req, res) => {
     const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
+        res.write('<!DOCTYPE html>');
+        res.write('<html lang="en-us">');
         res.write('<head>');
         res.write('<title>My Awesome Page</title>');
         res.write('<style>');
-        res.write('body { width: 600px; margin: 50 auto; font: 16px Arial, sans-serif; text-align: center; }');
-        res.write('input { height: 30px; width: 50%; margin: 10 auto}');
-        res.write('button { color: white; background-color: navy; border: none; display: block; height: 30px; width: 50%; margin: 0 auto; border-radius: 5px}')
+        res.write('body { width: 600px; margin: 50px auto; font: 16px Arial, sans-serif; text-align: center; }');
+        res.write('input { height: 30px; width: 50%; margin: 10px auto}');
+        res.write('button { color: white; background-color: navy; border: none; display: block; height: 30px; width: 50%; margin: 0px auto; border-radius: 5px}')
         res.write('</style>');
         res.write('</head>');
         res.write('<body>');
         res.write('<h1>This is my awesome website!</h1>');
-        res.write('<h2>Would you like to sign up?')
+        res.write('<h2>Would you like to sign up?</h2>');
         res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Sign Up</button></form>');
         res.write('</body>');
         res.write('</html>');
@@ -26,7 +27,8 @@ const routesHandler = (req, res) => {
         const usersData = fs.readFileSync('users.txt').toString();
         const users = usersData.split(',');
         res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
+        res.write('<!DOCTYPE html>');
+        res.write('<html lang="en-us">');
         res.write('<head>');
         res.write('<title>My Awesome Page</title>');
         res.write('<style>');
