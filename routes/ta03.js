86fdda14cc013b2fffdb72e9146e22f55fc962bd
Warner-Felicia <foodleswarner@gmail.com>
2021-09-29 15:40:43 -0400

    completed TA03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,13 +1,31 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+let items = [];
+const url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+  let settings = {
+    method: "Get"
+  };
+
+fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+      try {
+        items = (JSON.parse(JSON.stringify(json)));
+      } catch (e) {
+        console.log(e);
+      }
+    });
 
 router.get('/', (req, res, next) => {
+  
   res.render('pages/ta03', {
     title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    path: '/ta03',
+    items: items
   });
 });
 
